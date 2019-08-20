5968e6cf3a4b61ec8543df96497dc90fb5b7c29c
Mariacruz4 <eloisa943@gmail.com>
2019-08-20 12:58:14 -0500

    Added table service layer

--- Restaurant/src/main/java/com/company/Restaurant/Dao/GuestsRepository.java
+++ Restaurant/src/main/java/com/company/Restaurant/Dao/GuestsRepository.java
@@ -3,5 +3,9 @@ package com.company.Restaurant.Dao;
 import com.company.Restaurant.Dto.Guests;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GuestsRepository extends JpaRepository<Guests, Integer> {
+    List<Guests> findByLastName(String lastName);
+    List<Guests> findByNumInParty(Integer numInParty);
 }
